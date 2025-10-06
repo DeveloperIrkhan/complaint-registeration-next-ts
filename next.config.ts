d616d97a70b2e73f215c388090cbc9a7ca5a50e4
DeveloperIRkhan <programmer.irfansha@hotmail.com>
2025-10-06 17:19:50 +0500

    fixing next.config.ts file bug

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-      optimizeCss: false, // 🔥 disable LightningCSS
-
+  experimental: {
+    optimizeCss: false // ✅ must be inside 'experimental'
+  },
+  swcMinify: true // optional, improves performance
 };
 
 export default nextConfig;
