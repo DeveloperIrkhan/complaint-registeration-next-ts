ed4c1d1d52f149a4c43021f90f86030bbcc77417
DeveloperIRkhan <programmer.irfansha@hotmail.com>
2025-11-05 13:02:11 +0500

    small chages

--- src/app/(client)/complaint-tracking/[trackingId]/page.tsx
+++ src/app/(client)/complaint-tracking/[trackingId]/page.tsx
@@ -30,9 +30,10 @@ const Page = () => {
   useEffect(() => {
     if (!trackingId || !data?.success) return;
     if (isLoading || isFetching) return;
+    data.registeredComplaint !== null &&
+      toast.success(data.message || "complaint Status");
 
     setComplaint(data.registeredComplaint ?? null);
-
     const assignedUser = users.find(
       (x) => x._id === data.registeredComplaint?.assignedTo
     );
@@ -57,111 +58,8 @@ const Page = () => {
     : -1;
   useEffect(() => {}, [complaint]);
   return (
-    // <div className="h-screen">
-    //   {(isLoading || isFetching) && <Spinner />}
-    //   <div className="w-full h-full flex justify-center items-center bg-linear-to-tl to-red-500 from-orange-300">
-    //     <div className="max-w-xl w-full bg-white rounded-2xl shadow-lg p-6">
-    //       <h1 className="text-2xl font-bold text-center text-red-500 mb-4">
-    //         Complaint Tracking
-    //       </h1>
-    //       {complaint != null ? (
-    //         <div className="mt-8">
-    //           <h2 className="text-lg font-semibold mb-4 text-gray-700">
-    //             Status:{" "}
-    //             <span className="text-red-600 capitalize">
-    //               {complaint.complaintStatus?.replace("_", " ")}
-    //             </span>
-    //           </h2>
-    //           {/* Progress Tracker */}
-    //           <div className="relative flex justify-between items-center mb-8 px-2">
-    //             {/* Base Line */}
-    //             <div className="absolute top-1/2 left-0 right-0 h-[3px] bg-gray-300 -translate-y-1/2 z-0 rounded-full"></div>
-
-    //             {/* Filled Line */}
-    //             <div
-    //               className="absolute top-1/2 left-0 h-[3px] bg-red-600 -translate-y-1/2 z-10 rounded-full transition-all duration-700"
-    //               style={{
-    //                 width: `${
-    //                   ((currentStatusIndex + 1) / statuses.length) * 100
-    //                 }%`
-    //               }}
-    //             ></div>
-
-    //             {statuses.map((status, index) => {
-    //               const isActive = index <= currentStatusIndex;
-    //               const isCurrent = index === currentStatusIndex;
-    //               return (
-    //                 <div
-    //                   key={status}
-    //                   className="flex flex-col items-center text-center w-full"
-    //                 >
-    //                   {/* Bullet */}
-    //                   <div
-    //                     className={`z-20 flex justify-center items-center w-6 h-6 rounded-full border-[3px] transition-all duration-500
-    //         ${
-    //           isActive
-    //             ? "bg-red-600 border-red-600"
-    //             : "bg-white border-gray-300"
-    //         }
-    //         ${isCurrent ? "scale-110 ring-4 ring-red-200" : ""}
-    //       `}
-    //                     style={{ transform: "translateY(18px)" }}
-    //                   ></div>
-
-    //                   {/* Label */}
-    //                   <p
-    //                     className={`text-xs  mt-6 capitalize ${
-    //                       isActive
-    //                         ? "text-red-600 font-semibold"
-    //                         : "text-gray-400"
-    //                     }`}
-    //                   >
-    //                     {status.replace("_", " ")}
-    //                   </p>
-    //                 </div>
-    //               );
-    //             })}
-    //           </div>
-    //           <hr className="my-3 border-gray-300" />
-    //           <p className="text-lg">Complaint details</p>
-    //           <div className="mt-6 bg-red-100 p-4 rounded-lg border border-blue-100">
-    //             <p className="text-sm text-gray-600">
-    //               <strong>Name:</strong> {complaint.name}
-    //             </p>
-    //             <p className="text-sm text-gray-600">
-    //               <strong>Email:</strong> {complaint.email}
-    //             </p>
-    //             <p className="text-sm text-gray-600">
-    //               <strong>Description:</strong> {complaint.complaint}
-    //             </p>
-    //             <p className="text-sm text-gray-600">
-    //               <strong>priority set:</strong> {complaint.priority}
-    //             </p>
-    //             <p className="text-sm text-gray-600">
-    //               <strong>Assign to:</strong>{" "}
-    //               {complaint.assignedTo ? (
-    //                 <>{username}</>
-    //               ) : (
-    //                 "not yet assigned to"
-    //               )}
-    //             </p>
-    //             <p className="text-sm text-gray-600">
-    //               <strong>Last Updated:</strong>{" "}
-    //               {complaint.updatedAt
-    //                 ? new Date(complaint.updatedAt).toLocaleString()
-    //                 : "Not available"}
-    //             </p>
-    //           </div>
-    //         </div>
-    //       ) : (
-    //         <p className="text-red-600 bg-red-300 rounded-lg w-full p-4">
-    //           no record found
-    //         </p>
-    //       )}
-    //     </div>
-    //   </div>
-    // </div>
     <div className="min-h-screen flex justify-center items-center bg-gradient-to-tl to-red-500 from-orange-300 px-2 sm:px-4">
+      {(isLoading || isFetching) && <Spinner />}
       <div className="w-full max-w-md sm:max-w-xl bg-white rounded-2xl shadow-lg p-4 sm:p-6">
         <h1 className="text-xl sm:text-2xl font-bold text-center text-red-500 mb-4">
           Complaint Tracking
