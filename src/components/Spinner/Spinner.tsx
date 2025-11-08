3778dcca23f6aa7a93b114a195af934b7d9e1ae5
DeveloperIRkhan <programmer.irfansha@hotmail.com>
2025-11-08 19:32:44 +0500

    `Removed unused code and updated image paths in various files`

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -8,13 +8,20 @@ const Spinner = () => {
       className="z-50 fixed inset-0 flex justify-center items-center 
                  bg-darkColor/70 backdrop-blur-sm"
     >
-      <div className="flex flex-col justify-center items-center text-2xl">
+      <div className="flex flex-col justify-center relative items-center text-2xl">
+        <Image
+          src={images.icon}
+          width={30}
+          height={30}
+          alt="Loading Spinner"
+          className="absolute z-10 w-15 h-15"
+        />
         <Image
           src={images.Spinner}
           width={100}
           height={100}
           alt="Loading Spinner"
-          className="w-36 h-36 animate"
+          className="w-28 h-28 relative z-20 animate"
         />
       </div>
     </div>
