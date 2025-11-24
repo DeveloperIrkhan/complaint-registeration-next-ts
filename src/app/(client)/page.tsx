47a6d745583eed1e86dda26e3ecf8db8dfe9121f
developerirkhan <dev.irfankhan.pk@outlook.com>
2025-11-24 12:39:15 +0500

    new chagnes

--- src/app/(client)/page.tsx
+++ src/app/(client)/page.tsx
@@ -4,15 +4,16 @@ import React, { useEffect } from "react";
 import { toast } from "react-toastify";
 import { images } from "../Images";
 import MainPageButtons from "@/components/MainPageButtons";
+import Link from "next/link";
 
 const page = () => {
   useEffect(() => {
     toast.success("Welcome to PRCS User!", {
-      autoClose: 5000
+      autoClose: 5000,
     });
   }, []);
   return (
-    <div className="min-h-screen flex flex-col md:flex-row  bg-custom-linear md:p-0">
+    <div className="relative min-h-screen flex flex-col md:flex-row  bg-custom-linear md:p-0">
       <div className="flex-1 flex items-center justify-center px-6 md:px-12">
         <div className="w-full max-w-sm">
           <div className="text-center my-10">
@@ -58,6 +59,16 @@ const page = () => {
             className="mx-auto h-9/12 shadow-2xl rounded-xl"
           />
         </div>
+      </div>
+      <div
+        className="absolute bottom-2 right-10 z-30 w-full bg-transparent 
+      border-transparent flex justify-end items-center border-t"
+      >
+        <span className="text-sm text-white/30">
+          <Link target="_blank" href={"https://developerirkhan.github.io/irfankhan-portfolio"}>
+            © {new Date().getFullYear()} Irfan Designs.
+          </Link>
+        </span>
       </div>
     </div>
   );
